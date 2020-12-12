fda3c5e3168dc206f93497d55e1c9241f3adc5ec
yliess86 <hatiyliess86@gmail.com>
2020-12-12 17:54:53 +0100

    Add Gradient Penalty

---
+++ paintstorch2/model/loss.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GradientPenalty(nn.Module):
+    def __init__(self, discriminator: nn.Module, λ: float) -> None:
+        super(GradientPenalty, self).__init__()
+        self.D = discriminator
+        self.λ = λ
+
+    def forward(
+        self,
+        real: torch.Tensor,
+        fake: torch.Tensor,
+        f: torch.Tensor,
+    ) -> torch.Tensor:
+        α = torch.rand()
+        
+        t = α * real - (1 - α) * fake
+        t.requires_grad = True
+        Dt = self.D(t, f)
+        
+        grads = torch.autograd.grad(
+            outputs=Dt,
+            inputs=t,
+            grad_outputs=torch.ones_like(Dt),
+            create_graph=True,
+            retain_graph=True,
+            only_inputs=True,
+        )[0]
+
+        return self.λ * ((grads.norm(2, dim=1) - 1) ** 2).mean()
