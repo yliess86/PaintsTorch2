dcc51f90f090836d5d57ed45a7c995e2b81f20c6
yliess86 <hatiyliess86@gmail.com>
2020-12-17 15:15:28 +0100

    Refacto and Options

--- kubeflow/__main__.py
+++ kubeflow/__main__.py
@@ -1,48 +1,55 @@
 from .dpt.helper import DVICContainerOperation as Container
 from .dpt.helper import DVICPipelineWrapper as Pipeline
+
+import argparse
 import uuid
 
-from argparse import ArgumentParser
 
-parser = ArgumentParser()
+PIPELINE_NAME = "PaintsTorch2"
+PIPELINE_DESC = "PaintsTorch2 Training Pipeline"
 
-# Constants
-PIPELINE_NAME       = "PaintsTorchV2"
-PIPELINE_DESC       = "PaintsTorchV2 Training Pipeline"
-EXP_ID              = str(uuid.uuid4()).replace('-', '_')
-NAMESPACE           = "dvic-kf"
-RUN_NAME            = None
-MOUNT_PATH          = "/data"
+DOCKER_IMG    = "yliess86/paintstorchv2:latest"
+EXP           = str(uuid.uuid4()).replace("-", "_")
+NAMESPACE     = "dvic-kf"
 
-# Data path
-DGX_DATA_PATH           = "/data/dl/PaintsTorchV2"
-DATA_MOUNT_PATH         = "/data"
+BASE_PATH     = "/data"
+MOUNT_PATH    = f"{BASE_PATH}/dl/PaintsTorch2"
 
-# Tensorbopard data
-DGX_TENSORBOARD_PATH    = "/data/dl/tensorboard/PaintsTorchV2"
-TB_PATH                 = "/tensorboard"
+DATASET_PATH  = f"{BASE_PATH}/dataset"
+CKPT_PATH     = f"{BASE_PATH}/checkpoints"
+
+TB_PATH       = "/tensorboard"
+DGX_TB_PATH   = f"{BASE_PATH}/dl/tensorboard/PaintsTorch2"
 
 
-# Pipeline 
-IMAGE_NAME          = "win32gg/paintstorchv2:latest"
-
-
-# Train arguments
-parser.add_argument("--latent_dim",   type=int, default=128)
-parser.add_argument("--capacity",     type=int, default=64)
-parser.add_argument("--epochs",       type=int, default=200)
-parser.add_argument("--batch_size",   type=int, default=32)
-parser.add_argument("--ngpu",         type=int, default=1) 
-
+parser = argparse.ArgumentParser()
+parser.add_argument("--docker_img",    type=str, default=DOCKER_IMG)
+parser.add_argument("--latent_dim",    type=int, default=128)
+parser.add_argument("--capacity",      type=int, default=64)
+parser.add_argument("--epochs",        type=int, default=200)
+parser.add_argument("--batch_size",    type=int, default=16)
+parser.add_argument("--n_gpu",         type=int, default=1)
 args = parser.parse_args()
 
-with Pipeline(PIPELINE_NAME, PIPELINE_DESC, RUN_NAME, EXP_ID, NAMESPACE) as pipeline:
-    train = Container(IMAGE_NAME, *f'--latent_dim {args.latent_dim} --capacity {args.capacity} --epochs {args.epochs} --batch_size {args.batch_size} --dataset {DATA_MOUNT_PATH}/dataset --checkpoints {DATA_MOUNT_PATH}/checkpoints --tensorboards {TB_PATH}'.split(' '),
-                                name = "train"
-            )
 
-    train.select_node().gpu(args.ngpu) 
-    train.mount_host_path(DATA_MOUNT_PATH, DGX_DATA_PATH) 
-    train.mount_host_path(TB_PATH, DGX_TENSORBOARD_PATH)
+with Pipeline(PIPELINE_NAME, PIPELINE_DESC, None, EXP, NAMESPACE) as pipeline:
+    train = Container(
+        args.docker_img,
+        " ".join((
+            f"--latent_dim {args.latent_dim}",
+            f"--capacity {args.capacity}",
+            f"--epochs {args.epochs}",
+            f"--batch_size {args.batch_size}",
+            f"--dataset {DATASET_PATH}",
+            f"--checkpoints {CKPT_PATH}",
+            f"--tensorboards {TB_PATH}",
+            f"--data_parallel",
+        )),
+        name = "train"
+    )
+
+    train.select_node().gpu(args.n_gpu) 
+    train.mount_host_path(BASE_PATH, MOUNT_PATH) 
+    train.mount_host_path(TB_PATH, DGX_TB_PATH)
 
     pipeline()
