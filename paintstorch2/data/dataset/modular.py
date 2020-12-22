56e59c5a8d558eadf7c00befa544c4c996763da2
yliess86 <hatiyliess86@gmail.com>
2020-12-22 17:25:34 +0100

    Add from_config to Modular Dataset

--- paintstorch2/data/dataset/modular.py
+++ paintstorch2/data/dataset/modular.py
@@ -7,6 +7,7 @@ from typing import NamedTuple
 
 import torch
 import torchvision.transforms as T
+import yaml
 
 
 class Modules(NamedTuple):
@@ -59,3 +60,41 @@ class ModularPaintsTorch2Dataset(PaintsTorch2Dataset):
         ], dim=0)
 
         return Data(artist_id, composition, hints, style, illustration)
+
+    @classmethod
+    def from_config(
+        cls, config: str, path: str, is_train: bool = False,
+    ) -> "ModularPaintsTorch2Dataset":
+        from paintstorch2.data import (
+            COLOR_SIMPLIFIERS,
+            HINTS_GENERATORS,
+            LINEART_GENERATORS,
+            MASK_GENERATORS,
+        )
+        
+        with open(config, "r") as f:
+            content = f.read()
+        data = yaml.load(content)
+
+        color_data = data["color"]
+        color = COLOR_SIMPLIFIERS[color_data["name"]](
+            **color_data.get("params", {})
+        )
+
+        hints_data = data["hints"]
+        hints = HINTS_GENERATORS[hints_data["name"]](
+            **hints_data.get("params", {})
+        )
+
+        lineart_data = data["lineart"]
+        lineart = LINEART_GENERATORS[lineart_data["name"]](
+            **lineart_data.get("params", {})
+        )
+        
+        mask_data = data["mask"]
+        mask = MASK_GENERATORS[mask_data["name"]](
+            **mask_data.get("params", {})
+        )
+
+        modules = Modules(color, hints, lineart, mask)
+        return cls(modules, path, is_train)
