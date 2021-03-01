5b0b292df96e63d69d787f424f0fd519bcdba118
yliess86 <hatiyliess86@gmail.com>
2021-03-01 11:33:57 +0100

    Fixed Onnx Conversion

--- evaluation/onnx.py
+++ evaluation/onnx.py
@@ -38,6 +38,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--features", type=int, default=32)
 parser.add_argument("--model",    type=str, required=True)
 parser.add_argument("--save",     type=str, required=True)
+parser.add_argument("--opset",    type=int, default=9)
 parser.add_argument("--bn",       action="store_true")
 args = parser.parse_args()
 
@@ -45,10 +46,10 @@ args = parser.parse_args()
 model = PaintsTorch2(args.features, args.model, "./models/i2v.pth", args.bn)
 model = model.eval()
 
-x, h = torch.randn((2, 4, 512, 512)), torch.randn((2, 4, 128, 128))
+x, h = torch.ones((2, 4, 512, 512)), torch.zeros((2, 4, 128, 128))
 x[:, :3] = (x[:, :3] - 0.5) / 0.5
 h[:, :3] = (h[:, :3] - 0.5) / 0.5
-y_torch = model(x, h).numpy()
+y_torch = model(x, h)[0].numpy().transpose((1, 2, 0)) * 0.5 + 0.5
 
 torch.onnx.export(
     model, (x, h), args.save,
@@ -61,12 +62,14 @@ torch.onnx.export(
     },
     do_constant_folding=True,
     export_params=True,
-    opset_version=11,
+    opset_version=args.opset,
     verbose=False,
 )
 
 x, h = x.numpy(), h.numpy()
 session = onnx.InferenceSession(args.save)
-y_onnx = session.run(["illustration"], {"input": x, "hints": h})[0]
+y_onnx = session.run(["illustration"], {"input": x, "hints": h})
+y_onnx = y_onnx[0][0].transpose((1, 2, 0)) * 0.5 + 0.5
 
-print("Pytorch == Onnx ?", np.allclose(y_torch, y_onnx))
+print("Onnx close to Pytorch?", np.allclose(y_torch, y_onnx))
+print("Diff Abs:", np.sum(np.abs(y_torch - y_onnx)))
